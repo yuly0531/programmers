97042bf597afc5392eedea48fed43aa769a5a2f3
yuly_KR <138667064+yuly0531@users.noreply.github.com>
2024-02-29 03:40:20 +0900

    commit

---
+++ Oracle/평균 일일 대여 요금 구하기.sql
@@ -0,0 +1,8 @@
+/*다음은 어느 자동차 대여 회사에서 대여중인 자동차들의 정보를 담은 CAR_RENTAL_COMPANY_CAR 
+테이블입니다. CAR_RENTAL_COMPANY_CAR 테이블은 아래와 같은 구조로 되어있으며, CAR_ID, CAR_TYPE,
+ DAILY_FEE, OPTIONS 는 각각 자동차 ID, 자동차 종류, 일일 대여 요금(원), 자동차 옵션 리스트를 
+ 나타냅니다.*/
+SELECT
+    round(avg(DAILY_FEE),0) "AVERAGE_FEE"
+    from CAR_RENTAL_COMPANY_CAR
+where CAR_TYPE = 'SUV'
