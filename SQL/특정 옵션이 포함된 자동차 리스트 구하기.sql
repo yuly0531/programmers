83dbc0b768a900df5bf4b4f75b01435e4c3406de
yuly_KR <138667064+yuly0531@users.noreply.github.com>
2024-02-29 03:05:47 +0900

    commit

--- SQL/특정 옵션이 포함된 자동차 리스트 구하기.sql
+++
@@ -1,7 +0,0 @@
-/*
-다음은 어느 자동차 대여 회사에서 대여중인 자동차들의 정보를 담은 CAR_RENTAL_COMPANY_CAR 테이블입니다. CAR_RENTAL_COMPANY_CAR 테이블은 아래와 같은 구조로 되어있으며, CAR_ID, CAR_TYPE, DAILY_FEE, OPTIONS 는 각각 자동차 ID, 자동차 종류, 일일 대여 요금(원), 자동차 옵션 리스트를 나타냅니다.
-*/
-SELECT *
-from CAR_RENTAL_COMPANY_CAR
-where OPTIONS like '%네비게이션%'
-order by CAR_ID desc
